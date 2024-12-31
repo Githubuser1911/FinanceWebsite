ef2229e5ffa5ecebb259d8ef51b59c54303fd479
Githubuser1911 <danek1911@gmail.com>
2024-12-31 01:59:46 -0800

    Message

--- my-react-app/src/Team.jsx
+++ my-react-app/src/Team.jsx
@@ -9,13 +9,13 @@ function Team(){
                 alignItems: 'center',
                 height: '100vh',
                 textAlign: 'center',
-                fontSize: '1.5rem', // Adjust the font size if needed
-                color: '#333' // Optional: Change text color
+                fontSize: '1.5rem', 
+                color: '#333' 
                 
             }}
         >
             <p>
-                CEO: Dane Kelii (business economics)
+                CEO: Dane Kelii
             </p>
         </div>
     )
